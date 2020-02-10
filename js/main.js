596ac55183ac34199f0e368b6d2ad22bd8fe8336
Martin Provost <martin@fansfinger.com>
2020-02-10 11:37:46 +0100

    update fevrier

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 
-// Navbar
+// slider logo
 
-
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
 
 // Logo
 
